e6846b1cb4c6afbdd9df1bff44525d8e29b97b80
hyun <tyel2910@naver.com>
2019-05-01 11:57:28 +0900

    ..

--- app/src/main/java/com/example/fragment_1/MainActivity.java
+++ app/src/main/java/com/example/fragment_1/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.fragment_1;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
